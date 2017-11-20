b3489673e5c8dbfdb8689323e67eabcb1cab4420
Alina Mackenzie <alimknz@gmail.com>
2017-11-19 22:44:31 -0600

    Add basic create and show methods for Collections

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,8 +1,24 @@
 class CollectionsController < ApplicationController
+	def index
+    @collections = Collection.all
+  end
+
+	def show
+		@collection = Collection.find(params[:id])
+	end
+
 	def new
 	end
 
 	def create
-		render plain: params[:collection].inspect
+		@collection = Collection.new(collection_params)
+
+	  @collection.save
+	  redirect_to @collection
 	end
+
+	private
+		def collection_params
+			params.require(:collection).permit(:title, :fandom)
+		end
 end
