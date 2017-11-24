60681c7a04eea9da51d40e0d8356cc50df742794
Alina Mackenzie <alimknz@gmail.com>
2017-11-24 13:20:46 -0600

    Add some sample content

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+fandoms = Fandom.create([
+  { name: 'Death Note', category: 'Anime/Manga' },
+  { name: 'Fullmetal Alchemist', category: 'Anime/Manga' }
+])
+
+collections = Collection.create([
+  { title: 'Top Ten Death Note Fanfics', fandom_id: fandoms.first.id }
+])
+
+fanworks = Fanwork.create([
+  { title: 'Broken Hallelujahs', link: 'https://www.fanfiction.net/s/7472292/1/Broken-Hallelujahs', fandom_id: fandoms.first.id }
+])
